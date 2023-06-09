46c334c9d18e5f7ce8f28a8c9b7b2455e5224a77
DEEPANSHUDDD <107494136+DEEPANSHUDDD@users.noreply.github.com>
2023-06-09 06:45:49 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,13 @@
 echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/AsmSafone/MusicPlayer /MusicPlayer
+git clone https://github.com/DEEPANSHUDDD/Music /Music
+
 echo ">> INSTALLING REQUIREMENTS..."
-cd /MusicPlayer
+
 pip3 install -U -r requirements.txt
 echo ">> STARTING MUSIC PLAYER USERBOT..."
-clear
+
+python3 main.py
+
 echo "
 #     #                        
 ##   ## #    #  ####  #  ####  
@@ -24,4 +27,4 @@ echo "
                                           
 MUSIC PLAYER USERBOT IS SUCCESSFULLY DEPLOYED!
 "
-python3 main.py
+
