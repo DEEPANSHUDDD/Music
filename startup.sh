530c809fd981a11aba9b1a2ebcda5a10163ce467
DEEPANSHUDDD <107494136+DEEPANSHUDDD@users.noreply.github.com>
2023-06-09 07:07:19 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,6 +3,7 @@ git clone https://github.com/DEEPANSHUDDD/Music /Music
 
 echo ">> INSTALLING REQUIREMENTS..."
 
+ls
 pip3 install -U -r requirements.txt
 echo ">> STARTING MUSIC PLAYER USERBOT..."
 
